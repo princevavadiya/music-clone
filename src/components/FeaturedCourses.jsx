e68852a4cac61f852b14a401d95f320d0a7b6ff4
prince <vavadiyaprince999@gmail.com>
2025-03-27 10:30:43 +0530

    code

--- src/components/FeaturedCourses.jsx
+++ src/components/FeaturedCourses.jsx
@@ -18,7 +18,7 @@ function FeaturedCourses() {
           {courseData.courses.filter((course) => course.isFeatured).map((course) => (
             <div key={course.id} className="flex justify-center">
               <BackgroundGradient
-                className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+                className="flex flex-col rounded-[22px]  bg-zinc-900 overflow-hidden h-full max-w-sm">
                 <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
                   <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
                   <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">{course.description}</p>
